0e4bf5771d75be28701011ce945b27fe381ad603
Joshua Navarro <joshua.navarro35@gmail.com>
2023-01-17 03:51:10 -0500

    nvim: Add auto colorscheme on init

--- .config/nvim/lua/plugins/config/themer.lua
+++ .config/nvim/lua/plugins/config/themer.lua
@@ -10,9 +10,43 @@ themer.setup({
   enable_installer = true,
 })
 
-vim.cmd[[highlight DiffAdd gui=none guifg=none guibg=#273732]]
-vim.cmd[[highlight DiffDelete gui=none guifg=none guibg=#3f2d32]]
-vim.cmd[[highlight DiffChange gui=none guifg=none guibg=#263549]]
-vim.cmd[[highlight DiffText gui=none guifg=none guibg=#813c3c]]
+-- Configure aweson initial theme - nordfont
+local cmd = vim.cmd
+local fn = vim.fn
+local themer_install_path = fn.stdpath("data").."/themer/nightfox.nvim"
+local group_name = "ThemerInit"
 
-vim.cmd[[colorscheme nordfox]]
+vim.api.nvim_create_autocmd("ChanOpen", {
+  group = vim.api.nvim_create_augroup(group_name, {}),
+  callback = function ()
+    local chans = vim.api.nvim_list_chans()
+    local themer_job_id
+
+    for _, value in ipairs(chans) do
+      if value.stream == "job" then
+        themer_job_id = value.id
+      end
+    end
+
+    fn.jobwait({themer_job_id})
+    cmd[[colorscheme nordfox]]
+    vim.api.nvim_del_augroup_by_name(group_name)
+  end,
+  once = true
+})
+
+if fn.empty(fn.glob(themer_install_path)) > 0 then
+  local themer_installer_utils = require "themer.modules.installer.utils"
+  local nightfox_theme = "EdenEast/nightfox.nvim"
+
+  themer_installer_utils.install(nightfox_theme)
+else
+  cmd[[colorscheme nordfox]]
+end
+
+cmd[[highlight DiffAdd gui=none guifg=none guibg=#273732]]
+cmd[[highlight DiffDelete gui=none guifg=none guibg=#3f2d32]]
+cmd[[highlight DiffChange gui=none guifg=none guibg=#263549]]
+cmd[[highlight DiffText gui=none guifg=none guibg=#813c3c]]
+
+
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,10 +1,11 @@
 local ensure_packer = function()
   local fn = vim.fn
+  local cmd = vim.cmd
   local install_path = fn.stdpath("data").."/site/pack/packer/start/packer.nvim"
   if fn.empty(fn.glob(install_path)) > 0 then
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#4c566a" })
     fn.system({"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path})
-    vim.cmd "packadd packer.nvim"
+    cmd "packadd packer.nvim"
     return true
   end
   return false
@@ -16,4 +17,3 @@ local plugins = require("plugins.config").plugins_init(packer_bootstrap)
 local packer_config = require("plugins.config.others").packer_init()
 
 require("packer").startup({ plugins, config = packer_config })
-
