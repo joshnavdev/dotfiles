2354663a7cbf6d61153eedf26795aaa41d1928bd
Joshua Navarro <joshua.navarro35@gmail.com>
2023-01-24 20:48:38 -0500

    add prettier

--- .config/nvim/lua/plugins/configs/null-ls.lua
+++ .config/nvim/lua/plugins/configs/null-ls.lua
@@ -1,33 +1,66 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = "BufReadPre",
-  dependencies = { "mason.nvim" },
-  opts = function ()
-    local null_ls = require "null-ls"
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "BufReadPre",
+    dependencies = { "mason.nvim" },
+    opts = function ()
+      local null_ls = require "null-ls"
 
-    return {
-      on_attach = function (client, _)
-        if client.server_capabilities.documentFormattingProvider then
-          local group = vim.api.nvim_create_augroup("Format", {})
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            group = group,
-            buffer = 0,
-            callback = function ()
-              vim.lsp.buf.format()
-            end,
-          })
-        end
-      end,
-      debug = true,
-      sources = {
-        null_ls.builtins.diagnostics.eslint_d.with({
-          diagnostics_format = "[#{c}] #{m} (#{s})"
-        }),
-        null_ls.builtins.diagnostics.jsonlint,
-        null_ls.builtins.diagnostics.stylelint,
-        null_ls.builtins.formatting.stylelint,
-        null_ls.builtins.formatting.prettierd,
+      local diagnostics = null_ls.builtins.diagnostics
+      local formatting = null_ls.builtins.formatting
+
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+      return {
+        on_attach = function (current_client, bufnr)
+          if current_client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({
+                  filter = function(client)
+                    -- only use null-ls for formatting instead of lsp server
+                    return client.name == "null-ls"
+                  end,
+                  bufnr = bufnr,
+                })
+              end,
+            })
+          end
+        end,
+        debug = true,
+        sources = {
+          diagnostics.eslint_d.with({
+            diagnostics_format = "[#{c}] #{m} (#{s})"
+          }),
+          diagnostics.jsonlint,
+          diagnostics.stylelint,
+          formatting.stylelint,
+        }
       }
-    }
-  end,
+    end,
+  },
+  {
+    "MunifTanjim/prettier.nvim",
+    event = "BufReadPre",
+    opts = {
+      bin = "prettier",
+      filetypes = {
+        "css",
+        "graphql",
+        "html",
+        "javascript",
+        "javascriptreact",
+        "json",
+        "less",
+        "markdown",
+        "scss",
+        "typescript",
+        "typescriptreact",
+        "yaml",
+      },
+    },
+  },
 }
