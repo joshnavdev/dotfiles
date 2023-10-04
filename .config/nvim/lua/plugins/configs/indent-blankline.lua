8534b659ebb1e29112f32ddb45068d1c9dcea181
Joshua Navarro <joshua.navarro35@gmail.com>
2023-10-04 02:26:25 -0500

    nvim: update some plugins

--- .config/nvim/lua/plugins/configs/indent-blankline.lua
+++ .config/nvim/lua/plugins/configs/indent-blankline.lua
@@ -1,14 +1,15 @@
 return {
   "lukas-reineke/indent-blankline.nvim",
   event = "BufReadPost",
+  main = "ibl",
   opts = {
-    show_end_of_line = true,
-    filetype_exclue = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+    exclude = {
+      filetypes = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" }
+    }
   },
-  config = function (_, opts)
+  config = function(_, opts)
     vim.opt.list = true
     vim.opt.listchars:append "eol:↴"
-
-    require("indent_blankline").setup(opts)
-  end
+    require("ibl").setup(opts)
+  end,
 }
