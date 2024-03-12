de09ea56b0f2e79d70bbb03bec17dba89f0c5748
Joshua Navarro <joshua.navarro35@gmail.com>
2024-03-12 01:26:30 -0500

    nvim: Add LazyGit

--- .config/nvim/lua/plugins/coding.lua
+++ .config/nvim/lua/plugins/coding.lua
@@ -46,4 +46,21 @@ return {
       },
     },
   },
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    keys = {
+      { "<leader>lg", "<Cmd>LazyGit<CR>", { desc = "LazyGit" } },
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
